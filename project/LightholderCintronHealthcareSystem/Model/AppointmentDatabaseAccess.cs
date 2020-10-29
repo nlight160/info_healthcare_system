bb0d48348b038cdb18cf1095f832067e9af0af2c
Justin Cintron <jcintro1@my.westga.edu>
2020-10-29 13:25:53 -0400

    Added update appointment. Need to fix TODO in update appointment.

--- project/LightholderCintronHealthcareSystem/Model/AppointmentDatabaseAccess.cs
+++ project/LightholderCintronHealthcareSystem/Model/AppointmentDatabaseAccess.cs
@@ -5,11 +5,19 @@ using MySql.Data.MySqlClient;
 
 namespace LightholderCintronHealthcareSystem.Model
 {
+    /// <summary>
+    /// Appointment Database access.
+    /// </summary>
     public class AppointmentDatabaseAccess
     {
         private const string ConStr = "server=160.10.25.16; port=3306; uid=cs3230f20j;" +
                                       "pwd=F1UgUzIjwlhLAQ9a;database=cs3230f20j;";
 
+        /// <summary>
+        /// Creates the appointment.
+        /// </summary>
+        /// <param name="a">a.</param>
+        /// <returns></returns>
         public bool CreateAppointment(Appointment a)
         {
 
@@ -44,6 +52,47 @@ namespace LightholderCintronHealthcareSystem.Model
             }
 
 
+        }
+
+        /// <summary>
+        /// Updates the appointment.
+        /// </summary>
+        /// <param name="originalAppointment">The original appointment.</param>
+        /// <param name="newAppointment">The new appointment.</param>
+        /// <returns></returns>
+        public bool updateAppointment(Appointment originalAppointment, Appointment newAppointment)
+        {
+
+            var originalAppointmentid = originalAppointment.Patient; //TODO make appointmentid
+
+            var patientid = newAppointment.Patient;
+            var date = newAppointment.AppointmentDateTime;
+            var doctorid = newAppointment.Doctor;
+            var description = newAppointment.Description;
+
+            try
+            {
+                using var conn = new MySqlConnection(ConStr);
+                conn.Open();
+                using var cmd = new MySqlCommand { Connection = conn };
+
+                var updateAppointment =
+                    "UPDATE `appointment` SET `patientid` = @patientid, `date` = @date, `doctorid` = @doctorid, `description` = @description WHERE `appointmentid` = @originalAppointmentid;";
+                cmd.CommandText = updateAppointment;
+                cmd.Parameters.AddWithValue("@patientid", patientid);
+                cmd.Parameters.AddWithValue("@date", date);
+                cmd.Parameters.AddWithValue("@doctorid", doctorid);
+                cmd.Parameters.AddWithValue("@description", description);
+                cmd.Parameters.AddWithValue("@originalAppointmentid", originalAppointmentid);
+
+                var confirmation = cmd.ExecuteNonQuery();
+                return confirmation == 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in the CreatePatient: " + ex.ToString());
+                return false;
+            }
         }
 
     }
