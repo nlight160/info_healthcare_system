1109e002b79beaa13fc00ba56139214bf8e14b09
nlight160 <nathanlightholder@gmail.com>
2020-11-15 09:32:07 -0500

    Added test and test order classes

---
+++ project/LightholderCintronHealthcareSystem/Model/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightholderCintronHealthcareSystem.Model.People
+{
+    public class Test
+    {
+        public string TestName { get; }
+        public string TestCode { get; }
+        public Date DatePerformed { get; set; }
+        public string TestResults { get; set; }
+        public bool IsNormal { get; set; }
+
+        public Test(string testName, string testCode)
+        {
+            if (string.IsNullOrEmpty(testName))
+            {
+                throw new ArgumentException("test name can not be null or empty", nameof(testName));
+            }
+            this.TestName = testName;
+
+            if (string.IsNullOrEmpty(testCode))
+            {
+                throw new ArgumentException("test code can not be null or empty", nameof(testCode));
+            }
+            this.TestCode = testCode;
+
+            this.DatePerformed = new Date("" + DateTime.Now.Year, "" + DateTime.Now.Month, "" + DateTime.Now.Day);
+            this.TestResults = "";
+            this.IsNormal = true;
+        }
+    }
+}
---
+++ project/LightholderCintronHealthcareSystem/Model/TestOrder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightholderCintronHealthcareSystem.Model.People;
+
+namespace LightholderCintronHealthcareSystem.Model
+{
+    public class TestOrder
+    {
+        public IList<Test> Order { get; }
+        public Date DateOrdered { get; set; }
+
+        public TestOrder(List<Test> testsToOrder)
+        {
+            if (testsToOrder == null || testsToOrder.Count == 0)
+            {
+                throw new ArgumentException("Tests to order can not be null or empty", nameof(testsToOrder));
+            }
+            this.Order = testsToOrder;
+
+            this.DateOrdered = new Date("" + DateTime.Now.Year, "" + DateTime.Now.Month, "" + DateTime.Now.Day);
+        }
+
+        //Gets the amount of tests with the matching name
+        //in the order
+        public int getTestAmount(string testName)
+        {
+            int count = 0;
+            foreach (var test in this.Order)
+            {
+                if (test.TestName.Equals(testName))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
